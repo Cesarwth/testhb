7b0cb89eb683816be08caee8a8a9600516be1eeb
Cesarwth <cesarwth@gmail.com>
2024-09-15 02:37:30 -0500

    Update changes test

--- src/test/java/com/hb/campaign/domain/model/PriceTest.java
+++ src/test/java/com/hb/campaign/domain/model/PriceTest.java
@@ -11,13 +11,10 @@ public class PriceTest {
 
     @Test
     public void testPriceEntityCreation() {
-        // Crear una instancia de Price
         Price price = new Price();
 
-        // Verificar que la instancia no es nula
         assertNotNull(price);
 
-        // Configurar valores
         price.setBrandId(1);
         price.setStartDate(LocalDateTime.of(2020, 6, 14, 0, 0));
         price.setEndDate(LocalDateTime.of(2020, 12, 31, 23, 59));
@@ -27,7 +24,6 @@ public class PriceTest {
         price.setPrice(35.50);
         price.setCurrency("EUR");
 
-        // Verificar los valores establecidos
         assertEquals(1, price.getBrandId());
         assertEquals(LocalDateTime.of(2020, 6, 14, 0, 0), price.getStartDate());
         assertEquals(LocalDateTime.of(2020, 12, 31, 23, 59), price.getEndDate());
